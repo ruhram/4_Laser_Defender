781890ca65aa5098d11cfa4094dc72b72f00e3db
Ruhram <rullyrgandadiwirya@gmail.com>
2021-03-10 13:52:58 +0700

    Clamping movement

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,10 +6,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float Xmin;
+    [SerializeField] float Xmax;
+    [SerializeField] float Ymin;
+    [SerializeField] float Ymax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector2(transform.position.x, transform.position.y);
+        setUpMoveBoundaries();
+         
+    }
+
+    private void setUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        Xmin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+        Xmax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+        Ymin = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y;
+        Ymax = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, 0)).y;
     }
 
     // Update is called once per frame
@@ -21,9 +36,11 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXpos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYpos = transform.position.y + deltaY;
-        transform.position = new Vector2(deltaX, deltaY);
+
+        var newXpos = Mathf.Clamp(transform.position.x + deltaX, Xmin, Xmax);
+        var newYpos = Mathf.Clamp(transform.position.y + deltaY, Ymin, Ymax);
+
+        transform.position = new Vector2(newXpos, newYpos);
     }
 }
