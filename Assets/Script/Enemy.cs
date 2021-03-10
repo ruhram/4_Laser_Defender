690647f3b25713871f5dca8ac0b1d107e6c93afc
Ruhram <rullyrgandadiwirya@gmail.com>
2021-03-10 23:43:12 +0700

    Make Enemy Shoot

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,20 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = .2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
