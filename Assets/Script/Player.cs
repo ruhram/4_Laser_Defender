4e117b53a9f32132486f40791ab29ee9033114f0
Ruhram <rullyrgandadiwirya@gmail.com>
2021-03-11 19:48:47 +0700

    fixing bug enemy and player projectile

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -81,12 +81,14 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
         ProcessHit(damageDealer);
     }
 
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if(health < 0)
         {
             Destroy(gameObject);
