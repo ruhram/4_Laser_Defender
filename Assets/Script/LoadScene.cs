8a010690725ffe96539def04ba3a2e8fe0ab6a1c
Ruhram <rullyrgandadiwirya@gmail.com>
2021-03-11 22:57:06 +0700

    load all scene

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Core Game");
+    }
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
